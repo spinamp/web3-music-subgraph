5f56f9bb3ac62c2bb9dbb581a6f7a462647f355b
Simon Emanuel Schmid <simon@edgeandnode.com>
2022-01-26 13:14:05 +0000

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,31 @@
+import {
+  AdminChanged as AdminChangedEvent,
+  BeaconUpgraded as BeaconUpgradedEvent,
+  Upgraded as UpgradedEvent
+} from "../generated/BeaconProxy /BeaconProxy "
+import { AdminChanged, BeaconUpgraded, Upgraded } from "../generated/schema"
+
+export function handleAdminChanged(event: AdminChangedEvent): void {
+  let entity = new AdminChanged(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.previousAdmin = event.params.previousAdmin
+  entity.newAdmin = event.params.newAdmin
+  entity.save()
+}
+
+export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
+  let entity = new BeaconUpgraded(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.beacon = event.params.beacon
+  entity.save()
+}
+
+export function handleUpgraded(event: UpgradedEvent): void {
+  let entity = new Upgraded(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.implementation = event.params.implementation
+  entity.save()
+}
